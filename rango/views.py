48e6460c0d2658709a297944069d831fb499cdea
Kendra Swanson <2436551S@student.gla.ac.uk>
2019-01-24 01:36:20 +0000

    ch 3-4

---
+++ rango/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    content_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+    return render(request, 'rango/index.html', context=content_dict)
+
+def about(request):
+    return render(request, 'rango/about.html')
